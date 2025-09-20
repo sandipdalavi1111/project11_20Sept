acd842fa3ba7f8b17465d7c657cb8bf708aedafd
sandipdalavi1111 <162698827+sandipdalavi1111@users.noreply.github.com>
2025-09-20 14:59:13 +0530

    commit2

--- JavaPractice/src/Demo1/Test10.java
+++ JavaPractice/src/Demo1/Test10.java
@@ -3,7 +3,7 @@ package Demo1;
 public class Test10 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+		System.out.println("Hello, Pune!");
 		printStarPyramid(5);
 	}
 
